60a8cc2df120b12d719cd35f8dc67d2b50d2069e
Anisha KC <anisha.kc7219@gmail.com>
2020-11-21 02:33:15 -0500

    initial commit

---
+++ src/components/dashboard/Notifications.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../App.css'
+
+const Notifications = () => {
+  return (
+    <div>
+      <p className="text">Notifications</p>
+    </div>
+  )
+}
+
+export default Notifications
